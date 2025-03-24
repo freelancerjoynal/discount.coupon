59b4b10133ff796821cb7e1ac5503ec642fce5e6
freelancerjoynal <developerjoynal@gmail.com>
2025-03-24 16:22:57 +0600

    google translator added to app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,29 @@
-import "./bootstrap";
-
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
-import { TranslationsProvider } from "./TranslationsContext";
-import "./i18n.js";
-// axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
 const appName = import.meta.env.VITE_APP_NAME || "Balash Coupons";
+
+// Function to dynamically load the Google Translate script
+const loadGoogleTranslate = () => {
+    const script = document.createElement("script");
+    script.src = "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    document.head.appendChild(script);
+};
+
+// Google Translate initialization function
+const googleTranslateElementInit = () => {
+    if (window.google) {
+        new window.google.translate.TranslateElement(
+            {
+                pageLanguage: "en", // Default language
+                includedLanguages: "en,es,fr,bn,ar", // Supported languages
+                layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+            },
+            "google_translate_element" // Target element ID
+        );
+    }
+};
 
 createInertiaApp({
     title: (title) => `${title} ${appName}`,
@@ -17,14 +34,20 @@ createInertiaApp({
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
+
         root.render(
-            <TranslationsProvider
-                translations={props.initialPage.props.translations}
-                locale={props.initialPage.props.locale}
-            >
-                <App {...props} />
-            </TranslationsProvider>
+            <>
+                <div id="app-container">
+                    <App {...props} />
+                    {/* Google Translate widget */}
+                    <div id="google_translate_element" style={{ marginTop: "20px" }}></div>
+                </div>
+            </>
         );
+
+        // Load Google Translate script and initialize
+        loadGoogleTranslate();
+        window.googleTranslateElementInit = googleTranslateElementInit;
     },
     progress: {
         color: "#4B5563",
