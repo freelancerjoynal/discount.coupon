1767f4caa09c74d7a612e8222a57e7e6f9473cf3
freelancerjoynal <developerjoynal@gmail.com>
2025-03-25 15:52:37 +0600

    footer part fixed to bottom

--- resources/js/Layouts/admin/Footer.jsx
+++ resources/js/Layouts/admin/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="footer" style={{background:"rgb(48, 48, 48)"}}>
+        <footer className="footer fixed bottom-0 w-full" style={{ background: "rgb(48, 48, 48)" }}>
             <p className="text-sm">
-                Copyright 2024 - Balash Coupons, Erbil.
+                Copyright {new Date().getFullYear()} - Balash Coupons, Erbil.
                 <a
                     className="text-primary-500 hover:underline"
                     href="https://getadmintoolkit.com"
@@ -13,8 +13,7 @@ export default function Footer() {
             </p>
 
             <p className="flex items-center gap-1 text-sm">
-
-
+                {/* Add any additional content here */}
             </p>
         </footer>
     );
